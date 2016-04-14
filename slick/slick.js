4bfbc9aa88988297f31efeeaa6204aad8089472e
Expert <fktjif@gmail.com>
2016-04-14 15:30:45 +0300

    BugFix: Offset calculation when slideCount == slidesToShow #1059

--- slick/slick.js
+++ slick/slick.js
@@ -1101,7 +1101,11 @@
         }
 
         if (_.options.centerMode === true && _.options.infinite === true) {
-            _.slideOffset += _.slideWidth * Math.floor(_.options.slidesToShow / 2) - _.slideWidth;
+            if (_.slideCount <= _.options.slidesToShow) {
+                _.slideOffset = ((_.slideWidth * Math.floor(_.options.slidesToShow)) / 2) - ((_.slideWidth * _.slideCount) / 2);
+            } else {
+                _.slideOffset += _.slideWidth * Math.floor(_.options.slidesToShow / 2) - _.slideWidth;
+            }
         } else if (_.options.centerMode === true) {
             _.slideOffset = 0;
             _.slideOffset += _.slideWidth * Math.floor(_.options.slidesToShow / 2);
